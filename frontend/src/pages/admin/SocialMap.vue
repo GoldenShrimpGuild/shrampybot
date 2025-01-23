1a6b965f7c14fdf489ead8293169ab577614b164
Aria Burrell <litui@litui.ca>
2025-01-22 23:05:08 -0700

    Social map tweaks.

--- frontend/src/pages/admin/SocialMap.vue
+++ frontend/src/pages/admin/SocialMap.vue
@@ -55,98 +55,72 @@ const sortedUsers = computed(() => {
           <th>Discord Username</th>
           <th>Twitch ID</th>
           <th>Twitch Channel</th>
-          <th>Type</th>
-          <th>YouTube</th>
+          <th>Team/Mast.</th>
           <th>Mastodon</th>
           <th>Bluesky</th>
+          <th>YouTube</th>
           <th>Steam</th>
           <th>Github</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="user in sortedUsers" :key="user.id">
-          <td><pre>{{ user.discord_user_id }}</pre></td>
+          <td>
+            <pre>{{ user.discord_user_id }}</pre>
+          </td>
           <td>
             <VaButton v-if="user.discord_username" round size="small" color="#000000" borderColor="discordBlurple">
               <VaIconDiscord style="margin-right: 0.2rem;" />
               {{ user.discord_username }}
             </VaButton>
           </td>
-          <td style="border-left: 2px solid #333333; text-align: right"><pre>{{ user.id }}</pre></td>
+          <td style="border-left: 2px solid #333333; text-align: right">
+            <pre>{{ user.id }}</pre>
+          </td>
           <td style="text-align: left">
-            <VaButton :href="'https://twitch.tv/' + user.login" 
-            round 
-            size="small" 
-            color="#000000"
-            borderColor="twitchPurple"
-            target="_blank">
+            <VaButton :href="'https://twitch.tv/' + user.login" round size="small" color="#000000"
+              borderColor="twitchPurple" target="_blank">
               <TwitchIcon style="margin-right: 0.2rem;" />
               {{ user.display_name }}
             </VaButton>
           </td>
           <td>
-            <VaChip v-if="user.broadcaster_type" outline size="small" color="gsgYellow">{{ user.broadcaster_type }}
-            </VaChip>
+            <VaIcon v-if="user.shrampybot_active == true" name="check" color="#00ff00"></VaIcon>
+            <!-- <VaChip v-if="user.broadcaster_type" outline size="small" color="gsgYellow">{{ user.broadcaster_type }}
+            </VaChip> -->
           </td>
           <td style="border-left: 2px solid #333333">
-            <VaButton v-if="user.youtube_username"
-            :href="'https://www.youtube.com/channel/' + user.youtube_user_id"
-            target="_blank"
-            size="small"
-            round
-            color="#000000"
-            borderColor="youtubeRed">
-              <YoutubeIcon style="margin-right: 0.2rem;" />
-              {{ user.youtube_username }}
-            </VaButton>
-          </td>
-          <td style="border-left: 2px solid #333333">
-            <VaButton v-if="user.mastodon_user_id" 
-            :href="'https://soc.gsg.live/@' + user.mastodon_user_id" 
-            round
-            color="#000000"
-            borderColor="mastodonLight"
-              target="_blank" size="small">
+            <VaButton v-if="user.mastodon_user_id" :href="'https://soc.gsg.live/@' + user.mastodon_user_id" round
+              color="#000000" borderColor="mastodonLight" target="_blank" size="small">
               <MastodonIcon style="margin-right: 0.2rem" />
               {{ user.mastodon_user_id }}
             </VaButton>
           </td>
           <td style="border-left: 2px solid #333333">
-            <VaButton v-if="user.bluesky_username"
-              :href="'https://bsky.app/profile/' + user.bluesky_username"
-              target="_blank"
-              color="#000000"
-              borderColor="blueskyBlue"
-              size="small"
-              outline
-              round>
-              <Bluesky style="margin-right: 0.2rem;"/>
+            <VaButton v-if="user.bluesky_username" :href="'https://bsky.app/profile/' + user.bluesky_username"
+              target="_blank" color="#000000" borderColor="blueskyBlue" size="small" outline round>
+              <Bluesky style="margin-right: 0.2rem;" />
               {{ user.bluesky_username ? '@' + user.bluesky_username : '' }}
             </VaButton>
           </td>
           <td style="border-left: 2px solid #333333">
-            <VaButton v-if="user.steam_username"
-              :href="'https://steamcommunity.com/id/' + user.steam_username"
-              target="_blank"
-              color="#000000"
-              borderColor="#ffffff"
-              size="small"
-              outline
-              round>
-              <SteamIcon style="margin-right: 0.2rem;"/>
+            <VaButton v-if="user.youtube_username" :href="'https://www.youtube.com/channel/' + user.youtube_user_id"
+              target="_blank" size="small" round color="#000000" borderColor="youtubeRed">
+              <YoutubeIcon style="margin-right: 0.2rem;" />
+              {{ user.youtube_username }}
+            </VaButton>
+          </td>
+          <td style="border-left: 2px solid #333333">
+            <VaButton v-if="user.steam_username" :href="'https://steamcommunity.com/id/' + user.steam_username"
+              target="_blank" color="#000000" borderColor="#ffffff" size="small" outline round>
+              <SteamIcon style="margin-right: 0.2rem;" />
               {{ user.steam_username }}
             </VaButton>
           </td>
           <td style="border-left: 2px solid #333333">
-            <VaButton v-if="user.github_username"
-              :href="'https://steamcommunity.com/id/' + user.steam_username"
-              target="_blank"
-              color="#000000"
-              borderColor="#ffffff"
-              size="small"
-              outline
-              round>
-              <VaIconGitHub style="margin-right: 0.2rem;"/>
+            <VaButton v-if="user.github_username" :href="'https://steamcommunity.com/id/' + user.steam_username"
+              target="_blank" color="#000000" borderColor="#ffffff" size="small" outline round>
+              <VaIconGitHub style="margin-right: 0.2rem;" />
               {{ user.github_username }}
             </VaButton>
           </td>
