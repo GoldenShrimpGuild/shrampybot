3dfd4a2f96d9d20e15b6b393f0949730c78b98f2
Aria Burrell <litui@litui.ca>
2025-04-19 15:03:35 -0600

    Fixed 404 redirect and added redirect params to login flow.
    - Passing 'state' query param through Discord OAuth2 flow

--- frontend/src/pages/auth/Login.vue
+++ frontend/src/pages/auth/Login.vue
@@ -1,44 +1,8 @@
 <template>
   <VaForm ref="form">
-    <!-- <h1 class="font-semibold text-4xl mb-4"></h1> -->
     <p class="text-base mb-4 leading-5">
       <ShrampybotLogo />
-      <!-- New to Vuestic? -->
-      <!-- <RouterLink :to="{ name: 'signup' }" class="font-semibold text-primary">Sign up</RouterLink> -->
     </p>
-    <!-- <VaInput
-      v-model="formData.email"
-      :rules="[validators.required, validators.email]"
-      class="mb-4"
-      label="Email"
-      type="email"
-    /> -->
-    <!-- <VaValue v-slot="isPasswordVisible" :default-value="false">
-      <VaInput
-        v-model="formData.password"
-        :rules="[validators.required]"
-        :type="isPasswordVisible.value ? 'text' : 'password'"
-        class="mb-4"
-        label="Password"
-        @clickAppendInner.stop="isPasswordVisible.value = !isPasswordVisible.value"
-      >
-        <template #appendInner>
-          <VaIcon
-            :name="isPasswordVisible.value ? 'mso-visibility_off' : 'mso-visibility'"
-            class="cursor-pointer"
-            color="secondary"
-          />
-        </template>
-</VaInput>
-</VaValue> -->
-
-    <!-- <div class="auth-layout__options flex flex-col sm:flex-row items-start sm:items-center justify-between">
-      <VaCheckbox v-model="formData.keepLoggedIn" class="mb-2 sm:mb-0" label="Keep me signed in on this device" />
-      <RouterLink :to="{ name: 'recover-password' }" class="mt-2 sm:mt-0 sm:ml-1 font-semibold text-primary">
-        Forgot password?
-      </RouterLink>
-    </div> -->
-
     <div class="flex justify-center mt-4">
       <VaButton class="w-full" color="discordBlurple" :href="GlobalStore.getDiscordOAuthUrl()">
         <VaIcon :component="VaIconDiscord" />
